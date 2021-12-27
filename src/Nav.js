ffd73c75e52e2c41ef31c9caacbd9a7deba864a7
Nisarg Dave <extrastuff58@yahoo.com>
2021-12-27 15:47:31 +0800

    Deployed to firebase

--- src/Nav.js
+++ src/Nav.js
@@ -30,7 +30,7 @@ function Nav() {
         <img
           onClick={() => navigate("/")}
           className="nav_logo"
-          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png"
           alt="Netflix Logo"
         />
         <img
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -10,7 +10,7 @@ function LoginScreen() {
       <div className="loginScreen_background">
         <img
           className="loginScreen_logo"
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          src="https://logos-world.net/wp-content/uploads/2020/04/Netflix-Logo.png"
           alt="Netflix Login Background"
         />
         <button className="loginScreen_button" onClick={() => setSignIn(true)}>
